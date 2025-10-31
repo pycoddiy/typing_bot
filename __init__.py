c82dc346ac9e7b7cdb0bfaa586ceb4d3356d74e5
pycoddiy <pycoddiy@gmail.com>
2025-10-31 15:15:32 -0500

    fix isort

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@
 
 __version__ = "0.1.0"
 
-import struct_editor, structured_capture, tyrec
+import struct_editor
+import structured_capture
+import tyrec
 
 __all__ = ["tyrec", "structured_capture", "struct_editor"]
